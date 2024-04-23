4af336d7b6713f8ddaae6477545a73cba826d7c0
Ismael Nyambu <133232846+Isma3l-19@users.noreply.github.com>
2024-04-23 10:10:48 +0000

    First Commit

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# basic flask application to server our routes
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
